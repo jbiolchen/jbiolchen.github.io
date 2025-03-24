be564836688d21fb1604641cfbeb8fc83b98334d
jbiolchen <kjchen@alum.mit.edu>
2025-03-24 15:14:24 -0400

    streamline page headers using js

---
+++ head.js
@@ -0,0 +1,14 @@
+// append \ to end of each line inside write()
+// produces default page title and icon
+
+document.write('\
+    <title>Kelly\'s Personal Website</title>\
+    <link rel="icon" href="/assets/dart-spinoff.jpg">\
+    <meta charset="UTF-8">\
+    <meta name="viewport" content="width=device-width, initial-scale=1">\
+    <link rel="stylesheet" href="/assets/css/main.css">\
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">\
+    <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?autoload=true&amp;skin=sunburst&amp;lang=css" defer=""></script>\
+');
---
+++ header.js
@@ -0,0 +1,12 @@
+// append \ to end of each line inside write()
+
+document.write('\
+    <h2></h2>\
+    <h1><a href="https://jbiolchen.github.io" class="bold topp left ">Views My Own</a></h1>\
+    <h2></h2>\
+    <p class="ital gray">The contents of this website unfortunately do not reflect the view of the National Institutes of Health, the Department of Health and Human Services, or the United States government.</p>\
+    <br>\
+    <a href="/pages/cv.html" class="button leftmar ">CV</a> <a href="/pages/faq.html" class="button ">FAQ</a> <a class="bold green">Email me!</a><a class="green"> kjchen [at] alum [dot] mit [dot] edu</a>\
+    <hr class="hr">\
+    <br>\
+');
