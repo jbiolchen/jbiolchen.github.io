a86f303bdf13dfcf82d3b1953e81ac6e75443683
jbiolchen <kjchen@alum.mit.edu>
2025-03-25 22:06:53 -0400

    added faq and damascus content

--- header.js
+++ header.js
@@ -6,7 +6,7 @@ document.write('\
     <h2></h2>\
     <p class="ital gray">The contents of this website unfortunately do not reflect the view of the National Institutes of Health, the Department of Health and Human Services, or the United States government.</p>\
     <br>\
-    <a href="/pages/cv.html" class="button leftmar ">CV</a> <a href="/pages/faq.html" class="button ">FAQ</a> <a class="bold green">Email me!</a><a class="green"> kjchen [at] alum [dot] mit [dot] edu</a>\
+    <a href="/pages/cv.html" class="button leftmar ">CV</a> <a href="/pages/faq.html" class="button ">FAQ</a> <a class="bold green" style="padding-left:10px">Email me!</a><a class="green"> kjchen [at] alum [dot] mit [dot] edu</a>\
     <hr class="hr">\
     <br>\
 ');
